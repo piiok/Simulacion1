d124c36c7bd2beba8731207be8cbefd45c3f71e0
Santiago Gómez <30384089+Sangohe@users.noreply.github.com>
2019-02-24 23:31:05 -0500

    arreglar roundRobin y agregar vista+logica pelota

--- logica.js
+++ logica.js
@@ -1,110 +1,285 @@
-function roundRobin(quantum, p1, p2) {
-  tiempoSalida1 = 0;
-  tiempoSalida2 = 0;
-  termino1 = false;
-  termino2 = false;
-  paso = 0;
-
-  labels = [];
-  tiempos = [[p1],[p2],[0],[0]];
-  console.log(tiempos);
-
-  console.log("Paso:",paso);
-  console.log("P1:",p1,"P2:",p2,"Quantum:",quantum)
-
-  while((p1 !== 0) || (p2 !== 0)){
-    paso++;
-    console.log("Paso:",paso)
-    //Proceso 1
-    if(p1 <= quantum){
-      tiempoSalida1 += p1;
-      paso1 = p1;
-      p1 = 0;
-      termino1 = true;
-    } else {
-      if(!termino2){
-        tiempoSalida1 += quantum;
-      }    
-      paso1 = quantum;  
-      p1 = p1 - quantum;
-    }
-    console.log("P1:",p1,"Tiempo de salida 1:",tiempoSalida1);
-    //Proceso 2
-    if(p2 <= quantum){
-      tiempoSalida2 = tiempoSalida2 + paso1 + p2;
-      aux1 = tiempoSalida1;
-      if(!termino1){
-        tiempoSalida1 = tiempoSalida2;
-      }
-      p2 = 0;
-      termino2 = true;
-    } else {
-      tiempoSalida2 = tiempoSalida2 + paso1 + quantum;
-      aux1 = tiempoSalida1;
-      if(!termino1){
-        tiempoSalida1 = tiempoSalida2;
-      }
-      p2 = p2 - quantum;
-    }
-    
-    console.log("P2:",p2,"Tiempo de salida 2:",tiempoSalida2);
-    datosCiclo = [p1, p2, aux1, tiempoSalida2];
-    tiempos[0].push(p1);
-    tiempos[1].push(p2);
-    tiempos[2].push(aux1);
-    tiempos[3].push(tiempoSalida2);
-  }
-  for(i = 0; i <= paso; i++){
-    numeroPaso = i.toString();
-    labels.push('Paso ' + i);
-  }
-
-  return [labels, tiempos];
+// Retorna un entero aleatorio entre min (incluido) y max (excluido)
+// ¡Usando Math.round() te dará una distribución no-uniforme!
+function getRandomInt(min = 1, max = 100) {
+	return Math.floor(Math.random() * (max - min)) + min;
+}
+function proceso2() {
+	var grafica = (document.getElementById('grafica1').innerHTML =
+		"<canvas id='canvas2' height='450' width='600'></canvas>");
+	var ctx1 = document.getElementById('canvas2').getContext('2d');
+	var capacidad = parseInt($('#capacidad').val());
+	var pisos = parseInt($('#pisos').val());
+	result = ascensor(capacidad, pisos);
+	var config1 = configuracion2([ result[0], result[1] ]);
+	$('#inicial').val(result[2]);
+	window.myLine2 = new Chart(ctx1, config1);
 }
 
-// function ascensor(capMax, canPisos){
-//   personasAscensor = Math.floor(Math.random() * capMax) + 1;
-//   console.log("El ascensor comenzo con:",personasAscensor,"personas.");
-//   labels = [];
-//   pasajerosPiso = [];
+function proceso3() {
+	var grafica = (document.getElementById('grafica2').innerHTML =
+		"<canvas id='canvas3' height='450' width='600'></canvas>");
+	var ctx1 = document.getElementById('canvas3').getContext('2d');
+	var config1 = configuracion3(altura());
+	window.myLine3 = new Chart(ctx1, config1);
+}
 
-//   for(i = 1; i <= canPisos; i++){
-//     numeroPiso = i.toString();
-//     labels.push('Piso ' + i);
-//     bajanPorPiso = Math.floor(Math.random() * personasAscensor) + 1;
-//     if(personasAscensor !== 0){
-//       personasAscensor -= bajanPorPiso;
-//     }
-//     console.log("Se bajaron",bajanPorPiso,"en el piso",i,"quedan:",personasAscensor);
-//     pasajerosPiso.push(personasAscensor);
-//   }
-//   console.log(labels);
-//   console.log(pasajerosPiso);
+function altura() {
+	var labels = [];
+	var a = [];
+	var p = [ 0.5, 0.25, 0.1 ];
+	var b = true;
+	for (i = 0; i < 3; i++) {
+		a[i] = [ 7 ];
+	}
+	//console.log(a);
+	var j = 0;
+	labels.push('' + j);
+	do {
+		j++;
+		labels.push('' + j);
+		for (i = 0; i < 3; i++) {
+			a[i].push(a[i][a[i].length - 1] * (1 - p[i]));
+			//console.log(a[i]);
+		}
+		if (a[0][a[0].length - 1] <= 0.0005 && a[1][a[1].length - 1] <= 0.0005 && a[2][a[2].length - 1] <= 0.0005) {
+			b = false;
+		}
+	} while (b);
+	return [ labels, a ];
+}
 
-// }
+function proceso(q) {
+	q = parseInt(q);
+	var grafica = (document.getElementById('grafica').innerHTML =
+		"<canvas id='canvas1' height='450' width='600'></canvas>");
+	var ctx1 = document.getElementById('canvas1').getContext('2d');
+	var p1 = getRandomInt();
+	var p2 = getRandomInt();
+	$('#p1').val('' + p1);
+	$('#p2').val('' + p2);
+	var config1 = configuracion(roundRobin(q, p1, p2));
+	window.myLine = new Chart(ctx1, config1);
+}
 
-function ascensor(capMax, canPisos){
-  personasAscensor = Math.floor(Math.random() * capMax) + 1;
-  personasInicial = personasAscensor;
-  console.log("El ascensor comenzo con:",personasAscensor,"personas.");
-  labels = [];
-  pasajerosPiso = [];
+function configuracion3(a) {
+	console.log(a);
+	return {
+		type: 'line',
+		data: {
+			labels: a[0],
+			datasets: [
+				{
+					backgroundColor: '#49147F',
+					borderColor: '#49147F',
+					label: 'Porcentaje 0,5',
+					fill: false,
+					data: a[1][0]
+				},
+				{
+					backgroundColor: '#EA38C9',
+					borderColor: '#EA38C9',
+					label: 'Porcentaje 0,25',
+					fill: false,
+					data: a[1][1]
+				},
+				{
+					backgroundColor: '#246420',
+					borderColor: '#246420',
+					label: 'Porcentaje 0,1',
+					fill: false,
+					data: a[1][2]
+				}
+			]
+		},
+		options: {
+			scales: {
+				yAxes: [
+					{
+						display: true,
+						ticks: {
+							min: 0
+						}
+					}
+				]
+			}
+		}
+	};
+}
 
-  for(i = 1; i <= canPisos; i++){
-    numeroPiso = i.toString();
-    labels.push('Piso ' + i);
-    if(personasAscensor !== 0){
-      quedanAscensor = Math.floor(Math.random() * personasAscensor);
-      console.log("Quedan:",quedanAscensor);
-      bajanPorPiso = personasAscensor - quedanAscensor;
-      personasAscensor = quedanAscensor;
-    }
-    console.log("Se bajaron",bajanPorPiso,"en el piso",i,"quedan:",personasAscensor);
-    pasajerosPiso.push(personasAscensor);
-  }
-  console.log(labels);
-  console.log(pasajerosPiso);
+function configuracion2(a) {
+	return {
+		type: 'horizontalBar',
+		data: {
+			labels: a[0],
+			// backgroundColor: color(window.chartColors.red).alpha(0.5).rgbString(),
+			datasets: [
+				{
+					backgroundColor: '#49147F',
+					label: 'Personas en el ascensor',
+					fill: false,
+					data: a[1]
+				}
+			]
+		},
+		options: {
+			scales: {
+				xAxes: [
+					{
+						display: true,
+						ticks: {
+							min: 0
+						}
+					}
+				]
+			}
+		}
+	};
+}
 
-  return [labels, pasajerosPiso, personasInicial];
+function configuracion(r) {
+	return {
+		type: 'bar',
+		data: {
+			labels: r[0],
+			datasets: [
+				{
+					label: 'Proceso 1',
+					backgroundColor: '#49147F',
+					fill: false,
+					data: r[1][0]
+				},
+				{
+					label: 'Proceso 2',
+					backgroundColor: '#EA38C9',
+					fill: false,
+					data: r[1][1]
+				},
+				{
+					label: 'T. de salida Proceso 1',
+					backgroundColor: '#6D0204',
+					fill: false,
+					data: r[1][2]
+				},
+				{
+					label: 'T. de salida Proceso 2',
+					backgroundColor: '#246420',
+					fill: false,
+					data: r[1][3]
+				}
+			]
+		},
+		options: {
+			scales: {
+				yAxes: [
+					{
+						display: true,
+						ticks: {
+							beginAtZero: true
+						}
+					}
+				]
+			}
+		}
+	};
+}
 
+window.onload = function() {
+	var q = getRandomInt();
+	$('#quantum').val('' + q);
+	proceso(q);
+	var cantidad = getRandomInt();
+	$('#capacidad').val('' + cantidad);
+	var pisos = getRandomInt();
+	$('#pisos').val('' + pisos);
+	proceso2();
+	proceso3();
+};
+
+function roundRobin(quantum, p1, p2) {
+	tiempoSalida1 = 0;
+	tiempoSalida2 = 0;
+	checkpoint = 0;
+	tiempos = [ [ p1 ], [ p2 ], [ tiempoSalida1 ], [ tiempoSalida2 ] ]; // se llena con p1, p2, ts1, ts2 respectivamente
+	labels = [];
+
+	termino1 = false;
+	termino2 = false;
+
+	while (p1 != 0 || p2 != 0) {
+		//Mirar si p1 > quantum
+		if (!termino1) {
+			if (p1 > quantum) {
+				avance = quantum;
+				tiempoSalida1 += quantum;
+				p1 -= quantum;
+			} else {
+				ultAvance1 = p1;
+				tiempoSalida1 += p1;
+				p1 = 0;
+				termino1 = true;
+			}
+		}
+		tiempos[0].push(p1);
+		tiempos[2].push(tiempoSalida1);
+
+		//mirar si p2 > quantum
+		if (!termino2) {
+			if (p2 > quantum) {
+				if (!termino1) {
+					tiempoSalida2 = tiempoSalida2 + avance + quantum;
+					tiempoSalida1 = tiempoSalida2;
+				} else {
+					tiempoSalida2 += ultAvance1;
+					ultAvance1 = 0;
+					tiempoSalida2 += quantum;
+				}
+				p2 -= quantum;
+			} else {
+				if (!termino1) {
+					tiempoSalida2 = tiempoSalida2 + avance + p2;
+					tiempoSalida1 = tiempoSalida2;
+				} else {
+					tiempoSalida2 += ultAvance1;
+					ultAvance1 = 0;
+					tiempoSalida2 += p2;
+				}
+				p2 = 0;
+				termino2 = true;
+			}
+		}
+		tiempos[1].push(p2);
+		tiempos[3].push(tiempoSalida2);
+	}
+	//llenar array de labels
+	for (i = 0; i < tiempos[0].length; i++) {
+		numeroPaso = i.toString();
+		labels.push('Paso ' + i);
+	}
+
+	return [ labels, tiempos ];
+}
+
+function ascensor(capMax, canPisos) {
+	personasAscensor = Math.floor(Math.random() * capMax) + 1;
+	personasInicial = personasAscensor;
+	console.log('El ascensor comenzo con:', personasAscensor, 'personas.');
+	labels = [];
+	pasajerosPiso = [];
+
+	for (i = 1; i <= canPisos; i++) {
+		numeroPiso = i.toString();
+		labels.push('Piso ' + i);
+		if (personasAscensor !== 0) {
+			quedanAscensor = Math.floor(Math.random() * personasAscensor);
+			console.log('Quedan:', quedanAscensor);
+			bajanPorPiso = personasAscensor - quedanAscensor;
+			personasAscensor = quedanAscensor;
+		}
+		console.log('Se bajaron', bajanPorPiso, 'en el piso', i, 'quedan:', personasAscensor);
+		pasajerosPiso.push(personasAscensor);
+	}
+	console.log(labels);
+	console.log(pasajerosPiso);
+
+	return [ labels, pasajerosPiso, personasInicial ];
 }
